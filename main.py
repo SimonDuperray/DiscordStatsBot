dd836d13966a4f6c5da5be339af90dc1feda0725
duperrsi <simon.duperray@reseau.eseo.fr>
2022-04-09 16:41:33 +0200

    :sparkles: time execution and file manager

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import discord
 import json
 import os
+import time
 
 from Models.Role import Role
 from Models.TextChannel import TextChannel
 from Models.VoiceChannel import VoiceChannel
 from discord.ext import commands
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv()
 TOKEN = os.getenv("DISCORD_TOKEN")
@@ -27,6 +29,7 @@ async def on_ready():
 
 @client.command(name="update")
 async def update(ctx):
+    global_time = time.time()
     if str(ctx.message.author) in AUTHORIZED:
         print("[debug] - Update Triggered")
 
@@ -37,14 +40,16 @@ async def update(ctx):
         # GET DATA
 
         # - general info about the guild and the owner
-        print("[debug] - Fetch general data (guild and owner)")
+        buffer_time = time.time()
 
         rst['guild_id'] = guild.id
         rst['guild_name'] = guild.name
         rst['guild_owner'] = str(guild.owner)
 
+        print(f"[debug] - Fetched general data (guild and owner) in {round(time.time() - buffer_time, 2)}s !")
+
         # - roles
-        print("[debug] - Fetch roles data")
+        buffer_time = time.time()
 
         roles = guild.roles
         roles_list = []
@@ -53,13 +58,17 @@ async def update(ctx):
 
         rst['roles'] = roles_list
 
+        print(f"[debug] - Fetched roles data in {round(time.time() - buffer_time, 2)}s !")
+
         # - members count
-        print("[debug] - Fetch members count")
+        buffer_time = time.time()
 
         rst['members_count'] = guild.member_count
 
+        print(f"[debug] - Fetched members count in {round(time.time() - buffer_time, 2)}s !")
+
         # - text and voice channels
-        print("[debug] - Fetch Text and Voice Channels")
+        buffer_time = time.time()
 
         text_channels = guild.text_channels
         text_channels_list = []
@@ -87,8 +96,10 @@ async def update(ctx):
 
         rst['voice_channels'] = voice_channels_list
 
+        print(f"[debug] - Fetched Text and Voice Channels in {round(time.time() - buffer_time, 2)}s !")
+
         # - members in voice channels
-        print("[debug] - Fetch Members in Voice Channels")
+        buffer_time = time.time()
 
         in_voice = {
             "total_count": 0,
@@ -106,37 +117,57 @@ async def update(ctx):
                     "channel_id": voice_buffer.id,
                     'members': members
                 })
-                await ctx.send(f"There are {len(voice_buffer.members)} members in {voice_buffer.name} channel: {members}")
+                await ctx.send(f"There are {len(voice_buffer.members)} members in {voice_buffer.name} "
+                               f"channel: {members}")
 
         rst['in_voice'] = in_voice
 
+        print(f"[debug] - Fetched Members in Voice Channels in {round(time.time() - buffer_time, 2)}s !")
+
         # - count messages
-        print("[debug] - Fetch counted messages")
+        buffer_time = time.time()
 
         count_messages = {
             "count": 0,
             "details": []
         }
-        for txt in rst['text_channels']:
-            count = 0
-            async for _ in client.get_channel(int(txt['id'])).history(limit=None):
-                count += 1
-            count_messages['details'].append({
-                "channel_name": txt['name'],
-                "channel_id": txt['id'],
-                "total": count
-            })
-            count_messages['count'] += count
+        # for txt in rst['text_channels']:
+        #     count = 0
+        #     async for _ in client.get_channel(int(txt['id'])).history(limit=None):
+        #         count += 1
+        #     count_messages['details'].append({
+        #         "channel_name": txt['name'],
+        #         "channel_id": txt['id'],
+        #         "total": count
+        #     })
+        #     count_messages['count'] += count
 
         rst['count_messages'] = count_messages
 
-        # write into json file
-        print("[debug] - Write into json file")
+        print(f"[debug] - Fetched counted messages in {round(time.time() - buffer_time, 2)}s !")
 
-        with open("./result.json", "w") as outfile:
+        # write into json file
+        buffer_time = time.time()
+
+        timestamp_filename = "extraction-"+str(datetime.timestamp(datetime.now()))+".json"
+        timestamp_path = "./"+timestamp_filename
+
+        with open(timestamp_path, "w") as outfile:
             json.dump(rst, outfile, indent=3)
 
-        print("> [debug] - All the data are successfully stored into json file!")
-        await ctx.send("All the data are successfully stored into json file!")
+        print(f"[debug] - Wrote into json file in {round(time.time() - buffer_time, 2)}s !")
+
+        global_end_time = round(time.time()-global_time, 2)
+        print(f"> [debug] - All the data are successfully stored into json file in {global_end_time}s !")
+        await ctx.send(f"All the data are successfully stored into json file in {global_end_time}s ! "
+                       f"I just have sent you your json file. ")
+        await ctx.message.author.send(file=discord.File(timestamp_path))
+
+        # delete timestamp file
+        if os.path.exists(timestamp_path):
+            os.remove(timestamp_path)
+        else:
+            print(f"[error] - The {timestamp_path} does not exist...")
+
 
 client.run(TOKEN)
